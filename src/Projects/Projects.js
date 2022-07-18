b1f1e26e19c3cfb6c2d31188463e2a098d077a45
Ilya Zhigarev <zhigarev@bk.ru>
2022-07-18 23:04:33 +0300

    project markup added

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,99 +1,125 @@
+import { Link } from "react-router-dom";
+import img14 from "../img/480x320/img14.jpg";
+import img16 from "../img/480x320/img16.jpg";
+import img25 from "../img/480x320/img25.jpg";
+// import amazonLogo from "../img/amazon.svg";
+
 function Projects() {
   return (
-    <div
-      className="container"
-      style={{ marginTop: '80px' }}
-    >
-      Здесь будет инфо о
-      проектах
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab beatae consequatur consequuntur
-        facere, illo molestiae molestias nobis odio officia praesentium quasi quos similique sit.
-        Cupiditate, delectus, dolore error explicabo fugiat laudantium magnam nemo placeat provident
-        quae quo quos soluta voluptatem! Accusantium dolor eaque, eligendi explicabo quis
-        recusandae! Aperiam at cupiditate dicta eligendi enim ex facilis harum, impedit incidunt
-        inventore minima nihil placeat quidem quos rem saepe vel, voluptatibus voluptatum! Adipisci
-        aliquid amet, architecto beatae blanditiis consequatur dolores facere itaque iusto maxime
-        omnis reprehenderit suscipit tempora totam ut velit veritatis? Aliquam aliquid aperiam
-        blanditiis consequuntur delectus dignissimos ea eius eveniet expedita, explicabo magnam
-        numquam obcaecati omnis perferendis placeat praesentium provident quae quas quos sapiente
-        suscipit veniam, voluptatem. Ad aliquid aperiam architecto autem dolore doloribus eius error
-        est eum fuga fugiat maxime nisi nostrum officia, porro saepe ullam voluptas voluptatibus. Ad
-        aliquid animi aspernatur assumenda consequuntur doloremque eius eligendi esse ex, facilis
-        fugit harum impedit iste necessitatibus nemo nesciunt perspiciatis placeat porro praesentium
-        quae quisquam quo quod reiciendis sapiente sunt. Explicabo modi, provident! Dicta doloremque
-        et iure magni quis? Beatae cum cupiditate distinctio, dolore dolores error explicabo harum
-        hic itaque iure magni minus molestias odit officiis rem repudiandae saepe tempore temporibus
-        tenetur, vero! Autem consequatur eius est hic molestias optio rerum! Aperiam aut blanditiis
-        et expedita quis saepe sit. Architecto consequuntur distinctio dolorum eaque eligendi
-        impedit laborum reprehenderit? Aliquam amet aperiam, autem corporis culpa deleniti
-        distinctio dolorem eaque earum facere fugiat id illo iure labore libero maiores neque nisi
-        non obcaecati perspiciatis provident quaerat quasi quisquam repellendus sapiente, sunt
-        suscipit totam ut velit voluptatum. Amet doloribus ducimus eum expedita fugiat incidunt
-        maxime nemo officia? Alias corporis dicta dignissimos dolor, doloremque, est harum inventore
-        nisi nostrum odio possimus quia tempore totam. Ab adipisci aperiam, asperiores aspernatur
-        consequatur culpa cum delectus doloremque doloribus dolorum eius esse et exercitationem
-        explicabo harum id illo iusto labore libero magni maxime praesentium quae sed sequi
-        voluptatibus! Accusamus alias animi aut autem beatae blanditiis commodi corporis cupiditate
-        dolore, dolorum eius, est ex explicabo illum in ipsa iusto laboriosam libero magnam
-        molestiae officiis optio perferendis quas quibusdam reiciendis repudiandae sint sunt totam
-        veniam vitae! Commodi dicta, dolor eius et exercitationem fugit id modi molestias nesciunt
-        numquam porro quisquam rerum soluta tempore totam. Ad animi, debitis dignissimos dolore
-        doloremque eos esse eum inventore iure iusto labore laborum magnam molestiae necessitatibus
-        obcaecati officiis qui recusandae repellat suscipit tenetur unde vero vitae! Animi cum
-        cupiditate distinctio, doloribus ea magni mollitia nihil non obcaecati possimus
-        reprehenderit, veritatis voluptate. Optio, quibusdam, repellat! Accusamus ad aperiam commodi
-        cumque, cupiditate dolore est eum expedita iste itaque labore laborum minus mollitia nostrum
-        omnis optio pariatur perspiciatis provident quaerat rem repellat, reprehenderit saepe sint
-        soluta tempora vero vitae. Deleniti dolore et laboriosam nemo nisi pariatur perferendis quas
-        quidem sed velit? Aliquam doloribus harum placeat? Amet deleniti, ducimus iste magni
-        obcaecati quam quidem voluptates! Aliquam aliquid amet cumque eaque fuga maiores nam quos
-        repudiandae, soluta veritatis. Commodi ducimus id impedit iure nisi non quod rerum! Ab at,
-        beatae cum cumque eveniet fuga illo libero magnam perspiciatis quaerat quasi sit sunt velit.
-        Animi, beatae debitis deleniti eligendi eum expedita facere fugiat ipsam ipsum itaque
-        laboriosam minima modi nemo nesciunt nisi omnis pariatur quam quasi qui quibusdam reiciendis
-        repellendus rerum, sequi, sunt velit veritatis vitae! A blanditiis dignissimos doloribus
-        enim esse, eveniet explicabo, fugiat fugit harum hic maxime minus natus nesciunt nisi
-        nostrum officia temporibus unde velit. Beatae consectetur molestiae neque, nesciunt quisquam
-        reprehenderit tempore ut voluptatibus. Ab accusamus aliquid consectetur delectus deleniti
-        in, incidunt inventore laborum magnam modi mollitia, quam sed, totam veritatis vero.
-        Asperiores dignissimos, ea eaque expedita odio odit officia quasi reprehenderit vel
-        voluptate. Architecto beatae corporis dicta eum eveniet hic laborum odio officiis, omnis
-        optio quo quos, soluta. A ad autem consectetur corporis deserunt dignissimos doloribus eos
-        est et ex fuga fugit illo inventore ipsa iure iusto magni, maxime molestias, nam obcaecati
-        odio optio provident quam quis quod, recusandae repellat rerum sint tempora temporibus ullam
-        unde veniam voluptates? Architecto doloribus dolorum earum fugit minima! A ab aliquid autem
-        beatae consectetur consequuntur corporis deleniti distinctio dolorem dolorum ducimus eius
-        enim esse est eveniet fugiat fugit impedit inventore iusto laboriosam maiores minima
-        necessitatibus officia, optio pariatur perferendis possimus quae quidem ratione, repudiandae
-        rerum sapiente sequi similique soluta sunt, tempora veniam. Animi asperiores aut deserunt
-        earum, itaque molestias nesciunt obcaecati officia optio quam quas reprehenderit tenetur
-        veniam. Adipisci deleniti ducimus expedita harum iste quos! Adipisci, aliquid beatae cum
-        deleniti est excepturi harum in, inventore iusto nesciunt nostrum pariatur possimus
-        reprehenderit tempora veniam veritatis voluptas. Animi aspernatur atque cupiditate dolore,
-        ea explicabo hic ipsum, nam necessitatibus non numquam omnis qui quia. Cumque dignissimos
-        maxime optio placeat praesentium quisquam quo quos similique vero voluptatibus? Ab aperiam
-        architecto asperiores at, atque blanditiis cupiditate, deserunt dicta dolorum expedita, id
-        in inventore ipsa laboriosam minima necessitatibus neque nobis nostrum numquam quasi quod
-        recusandae repellat sit veniam vero. Alias aliquam, consequuntur dignissimos exercitationem
-        ipsam libero sint voluptatum. Debitis, ducimus earum eos id natus voluptatibus. Ad amet
-        autem consequuntur cumque debitis dolore dolorem est illum iusto labore modi nisi nobis
-        omnis perferendis, quaerat ratione sed similique vel, voluptates voluptatibus. Beatae
-        commodi dolorem ea eveniet facilis laborum magni, quos rerum tenetur! Beatae est ex magni
-        obcaecati quas quasi quod ullam ut. Amet aspernatur, consequuntur cum deserunt dolores,
-        eligendi fugit hic impedit libero maxime minus officia quia repellendus veritatis
-        voluptates. A ad alias assumenda aut dolore eos inventore iusto, magnam obcaecati odit
-        pariatur porro, repudiandae rerum sequi ullam. Ipsa magnam quasi repellendus voluptatum? A
-        accusamus ad aliquam assumenda at atque, blanditiis culpa debitis eius eveniet
-        exercitationem explicabo fugiat minima odio, pariatur, tempore velit voluptate? Eligendi,
-        facere facilis hic magni mollitia numquam obcaecati! Ad animi ea excepturi explicabo illo,
-        ipsa iusto odio officiis omnis optio placeat porro praesentium quasi quis quos rem, repellat
-        tempora vero! Ad adipisci animi at autem culpa cumque dignissimos dolor eius, error ex id
-        illum inventore ipsa iure labore maiores obcaecati omnis quibusdam quisquam ratione
-        recusandae reiciendis sunt tempore totam veritatis. Cupiditate natus officiis quaerat
-        repudiandae totam.
+    <div className="container content-space-2 content-space-lg-3">
+      <div className="w-md-75 w-lg-50 text-center mx-md-auto mb-5 mb-md-9">
+        <h2>
+          Наши проекты
+        </h2>
       </div>
+
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 mb-5">
+        <div className="col mb-5">
+          <div className="card h-100">
+            <img
+              className="card-img-top"
+              src={img14}
+              alt="7777"
+            />
+
+            <div className="card-body">
+              {/* <div className="mb-3"> */}
+              {/*  <img */}
+              {/*    className="avatar avatar-lg avatar-4x3" */}
+              {/*    src={amazonLogo} */}
+              {/*    alt="Logo" */}
+              {/*  /> */}
+              {/* </div> */}
+              <p className="card-text">
+                <h3>Наш первый проект</h3>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, porro.
+              </p>
+            </div>
+
+            <div className="card-footer pt-0">
+              <Link className="card-link" to="/">
+                Подробнее
+                <i
+                  className="bi-chevron-right small ms-1"
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="col mb-5">
+          <div className="card h-100">
+            <img
+              className="card-img-top"
+              src={img16}
+              alt="7777"
+            />
+
+            <div className="card-body">
+              {/* <div className="mb-3"> */}
+              {/*  <img */}
+              {/*    className="avatar avatar-lg avatar-4x3" */}
+              {/*    src={amazonLogo} */}
+              {/*    alt="Logo" */}
+              {/*  /> */}
+              {/* </div> */}
+              <p className="card-text">
+                <h3>Наш второй проект</h3>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, vitae!
+              </p>
+            </div>
+
+            <div className="card-footer pt-0">
+              <Link className="card-link" to="/">
+                Подробнее
+                <i
+                  className="bi-chevron-right small ms-1"
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="col mb-5">
+          <div className="card h-100">
+            <img
+              className="card-img-top"
+              src={img25}
+              alt="7777"
+            />
+
+            <div className="card-body">
+              {/* <div className="mb-3"> */}
+              {/*  <img */}
+              {/*    className="avatar avatar-lg avatar-4x3" */}
+              {/*    src={amazonLogo} */}
+              {/*    alt="Logo" */}
+              {/*  /> */}
+              {/* </div> */}
+              <p className="card-text">
+                <h3>Наш третий проект</h3>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, ex.
+              </p>
+            </div>
+
+            <div className="card-footer pt-0">
+              <Link className="card-link" to="/">
+                Подробнее
+                <i
+                  className="bi-chevron-right small ms-1"
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      {/* <div className="text-center"> */}
+      {/*  <Link className="btn btn-outline-primary" to="/">Все партнеры</Link> */}
+      {/* </div> */}
     </div>
+
   );
 }
 
