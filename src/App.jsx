8d95d452ffdd0003869b87d0ee6acf64de04d0f4
Ilya Zhigarev <zhigarev@bk.ru>
2022-07-21 16:47:49 +0300

    components disconnected

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import About from "./About/About";
-import Projects from "./Projects/Projects";
+// import Projects from "./Projects/Projects";
 import NavBar from "./NavBar/NavBar";
-import News from "./News/News";
-import Volunteers from "./Volunteers/Volunteers";
-import Contacts from "./Contacts/Contacts";
-import Join from "./Join/Join";
-import Footer from "./Footer/Footer";
-import Project1 from "./Projects/Project1";
+// import News from "./News/News";
+// import Volunteers from "./Volunteers/Volunteers";
+// import Contacts from "./Contacts/Contacts";
+// import Join from "./Join/Join";
+// import Footer from "./Footer/Footer";
+// import Project1 from "./Projects/Project1";
 
 function App() {
   return (
@@ -22,56 +22,56 @@ function App() {
             </div>
           )}
         />
-        <Route
-          path="/projects"
-          element={(
-            <div>
-              <Projects />
-            </div>
-          )}
-        />
-        <Route
-          path="/projects/1"
-          element={(
-            <div>
-              <Project1 />
-            </div>
-          )}
-        />
-        <Route
-          path="/news"
-          element={(
-            <div>
-              <News />
-            </div>
-          )}
-        />
-        <Route
-          path="/volunteers"
-          element={(
-            <div>
-              <Volunteers />
-            </div>
-          )}
-        />
-        <Route
-          path="/contacts"
-          element={(
-            <div>
-              <Contacts />
-            </div>
-          )}
-        />
-        <Route
-          path="/join"
-          element={(
-            <div>
-              <Join />
-            </div>
-          )}
-        />
+        {/*  <Route */}
+        {/*    path="/projects" */}
+        {/*    element={( */}
+        {/*      <div> */}
+        {/*        <Projects /> */}
+        {/*      </div> */}
+        {/*    )} */}
+        {/*  /> */}
+        {/*  <Route */}
+        {/*    path="/projects/1" */}
+        {/*    element={( */}
+        {/*      <div> */}
+        {/*        <Project1 /> */}
+        {/*      </div> */}
+        {/*    )} */}
+        {/*  /> */}
+        {/*  <Route */}
+        {/*    path="/news" */}
+        {/*    element={( */}
+        {/*      <div> */}
+        {/*        <News /> */}
+        {/*      </div> */}
+        {/*    )} */}
+        {/*  /> */}
+        {/*  <Route */}
+        {/*    path="/volunteers" */}
+        {/*    element={( */}
+        {/*      <div> */}
+        {/*        <Volunteers /> */}
+        {/*      </div> */}
+        {/*    )} */}
+        {/*  /> */}
+        {/*  <Route */}
+        {/*    path="/contacts" */}
+        {/*    element={( */}
+        {/*      <div> */}
+        {/*        <Contacts /> */}
+        {/*      </div> */}
+        {/*    )} */}
+        {/*  /> */}
+        {/*  <Route */}
+        {/*    path="/join" */}
+        {/*    element={( */}
+        {/*      <div> */}
+        {/*        <Join /> */}
+        {/*      </div> */}
+        {/*    )} */}
+        {/*  /> */}
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
