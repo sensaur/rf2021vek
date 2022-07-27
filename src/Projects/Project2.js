2a237503d6633b152b4c38a95928cc6b735d2a21
Ilya Zhigarev <zhigarev@bk.ru>
2022-07-27 13:56:03 +0300

    Орнелла

---
+++ src/Projects/Project2.js
@@ -0,0 +1,87 @@
+// import wave from "../img/wave-pattern-light.svg";
+import { useEffect } from "react";
+import img14 from "../img/900x450/img14.jpg";
+
+function Project2() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [])
+  return (
+    <div
+      className="container"
+      style={{ marginTop: '90px' }}
+    >
+      <div
+        className="bg-dark rounded-2 mx-3 mx-xl-1"
+      >
+        <div
+          className="container-xl container-fluid content-space-1 content-space-md-2 px-4 px-md-8 px-lg-10"
+        >
+          <div className="px-3" />
+          <div className="w-md-75 w-lg-50 text-center mx-md-auto mb-5 mb-md-9">
+            <span className="text-cap text-white-70">Второй проект</span>
+            <h2 className="text-white">
+              Помогаем Lorem ipsum dolor sit amet, consectetur.
+            </h2>
+          </div>
+          <div className="card h-100">
+            <img
+              className="card-img-top"
+              src={img14}
+              alt="dobro"
+            />
+          </div>
+        </div>
+      </div>
+      <h3 className="py-3 mx-1">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi,
+        quaerat.
+      </h3>
+      <p className="py-1 mx-1">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda at atque
+        consequuntur et ipsa iste iusto labore maxime natus nostrum, perferendis placeat quasi quia
+        similique sint vitae. Aspernatur commodi dignissimos, enim et inventore laboriosam maiores,
+        maxime, molestiae mollitia quisquam vero voluptatibus. Itaque optio provident quo suscipit
+        tempore. Accusamus, aliquam aliquid cupiditate exercitationem hic laudantium mollitia
+        necessitatibus nemo placeat recusandae tempora voluptate, voluptatibus. Accusamus animi
+        aperiam aut autem beatae corporis doloremque earum eius eligendi est, hic illum incidunt
+        iste molestias necessitatibus neque nulla obcaecati omnis optio, porro possimus quam quidem
+        quo, ratione recusandae sed tempore tenetur ut vero voluptates! Consequuntur?
+      </p>
+      <h3 className="mx-1">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi,
+        quaerat.
+      </h3>
+      <p className="py-1 mx-1">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt eligendi maiores, minus
+        nostrum officia quia quod unde? A assumenda dolor, dolorem error eum excepturi illum maxime
+        nostrum omnis! Ab accusamus, aliquid animi asperiores autem corporis cupiditate deleniti
+        doloribus eaque eius est incidunt ipsum itaque laborum maiores minus, molestias odio
+        pariatur perferendis perspiciatis quae quaerat quam quas quasi quo quod rem similique
+        suscipit tenetur ullam vero voluptas voluptates voluptatibus? A dicta ipsa libero
+        necessitatibus provident quidem quod soluta veritatis? Adipisci, animi consequatur ducimus
+        eaque provident quo reiciendis veniam. A atque blanditiis culpa, deleniti minima
+        necessitatibus nostrum nulla quia quis quos rem sed voluptatibus voluptatum. Consequuntur
+        deserunt distinctio enim et expedita explicabo illum laboriosam maiores minus mollitia
+        nesciunt nihil nisi nostrum odit, praesentium quae quis, quisquam quo sequi sit temporibus
+        ullam voluptatibus? Aspernatur beatae dolores eos labore quaerat tenetur totam. A ab
+        accusamus amet animi architecto consequatur cupiditate deleniti dolor dolorem dolores
+        dolorum, earum error eum eveniet facere fuga incidunt inventore labore laudantium maiores
+        maxime minus nam, necessitatibus nisi officiis praesentium provident quae quasi qui quidem
+        reiciendis repudiandae similique sunt tenetur unde voluptate voluptatum! Est, iusto
+        obcaecati possimus quam sed suscipit. Cum delectus dolore doloremque explicabo id magni
+        minima perspiciatis placeat porro, quas qui quos reiciendis, sapiente similique sit suscipit
+        voluptate! A aspernatur aut deserunt doloremque enim eos eveniet facilis illo, inventore
+        ipsam libero neque odit quibusdam quis quod ratione rem, repellendus vero? Aliquam
+        asperiores aspernatur quaerat? A amet, animi aspernatur autem blanditiis consequatur
+        excepturi facere itaque laboriosam libero nam nemo nihil porro quaerat sequi suscipit
+        voluptas voluptate? Accusantium dolor est sapiente. Accusantium asperiores debitis
+        distinctio doloribus fugiat hic, illo mollitia odit omnis provident quae quis quo quos saepe
+        similique suscipit tenetur voluptatem! Doloribus, omnis, porro. Autem, beatae dolorum error
+        et, ipsam iste labore minima natus obcaecati optio ratione sed voluptatibus voluptatum?
+      </p>
+    </div>
+  );
+}
+
+export default Project2;
