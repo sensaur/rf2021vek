bef937cd5fa83e445e9b0d007385d0ed193578f6
Ilya Zhigarev <zhigarev@bk.ru>
2022-07-27 16:05:41 +0300

    context attempt 2

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+import UsersContextProvider from './context'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <UsersContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </UsersContextProvider>
   </React.StrictMode>,
 );
